e4f2c46eecfbd4d2bdfdc7575e45713d498b0468
Christine Contreras <christineec23@yahoo.com>
2021-04-15 18:59:20 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function superbowlWin(array) {
 
+    let found = array.find( value => value.result === "W")
+      
+    if (found) {
+        return found.year;
+    }
+}
+
+//THESE DONT WORK
+// function superbowlWin(array) {
+//     let found = array.find( value => {
+//         if(value.result === "W") {
+//             return value.year
+//         }
+//     })
+//       return found;
+// }
+
+
+// function superbowlWin(array) {
+//     let found = array.find( value => {
+//         if(value.result === "W") {
+//             return true
+//         }
+//     })
+//       return found.year;
+// }
